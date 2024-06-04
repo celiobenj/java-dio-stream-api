dd178b9c8a1041067a01e1d6e75231d286734886
celiob_ <celiolimabenjamim@gmail.com>
2024-06-03 22:29:04 -0400

    consumer

---
+++ stream-java-api/src/functional_interface/consumer/ConsumerExample.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.List;
+// import java.util.function.Consumer;
+
+public class ConsumerExample {
+    public static void main(String[] args) {
+        // Criar uma lista de números inteiros
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // Usar o Consumer com expressão lambda para imprimir números pares
+        // ! Consumer<Integer> imprimirNumeroPar = numero -> {
+        // ! if (numero % 2 == 0) {
+        // ! System.out.println(numero);
+        // ! }
+        // ! };
+
+        // Usar o Consumer para imprimir números pares no Stream
+
+        // ! numeros.stream().forEach(imprimirNumeroPar);
+
+        //? numeros.stream().forEach(new Consumer<Integer>() {
+        //?     @Override
+        //?     public void accept(Integer n) {
+        //?         if (n % 2 == 0)
+        //?             System.err.println(n);
+        //?     }
+        //? });
+
+        numeros.forEach(n -> { if (n % 2 == 0) System.out.println(n); });
+    }
+}
