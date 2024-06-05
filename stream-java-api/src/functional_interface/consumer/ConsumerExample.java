686de79d7b27a62695325c1d5d9ebecf4a2dc8b3
celiob_ <celiolimabenjamim@gmail.com>
2024-06-04 20:52:38 -0400

    update

--- stream-java-api/src/functional_interface/consumer/ConsumerExample.java
+++ stream-java-api/src/functional_interface/consumer/ConsumerExample.java
@@ -26,6 +26,9 @@ public class ConsumerExample {
         //?     }
         //? });
 
-        numeros.forEach(n -> { if (n % 2 == 0) System.out.println(n); });
+        // numeros.forEach(n -> { if (n % 2 == 0) System.out.println(n); });
+        numeros.stream()
+            .filter(n -> n % 2 == 0)
+            .forEach(System.out::println);
     }
 }
--- stream-java-api/src/functional_interface/predicate/PredicateExample.java
+++ stream-java-api/src/functional_interface/predicate/PredicateExample.java
@@ -2,7 +2,7 @@ package functional_interface.predicate;
 
 import java.util.Arrays;
 import java.util.List;
-import java.util.function.Predicate;
+// import java.util.function.Predicate;
 
 /**
  * Representa uma função que aceita um argumento do tipo T e retorna um valor booleano (verdadeiro oufalso).
@@ -14,12 +14,17 @@ public class PredicateExample {
     List<String> palavras = Arrays.asList("java", "kotlin", "python", "javascript", "c", "go", "ruby");
 
     // Criar um Predicate que verifica se a palavra tem mais de 5 caracteres
-    Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
+    // Predicate<String> maisDeCincoCaracteres = palavra -> palavra.length() > 5;
 
     // Usar o Stream para filtrar as palavras com mais de 5 caracteres e, em seguida,
     // imprimir cada palavra que passou no filtro
+//     palavras.stream()
+//         .filter(maisDeCincoCaracteres)
+//         .forEach(System.out::println);
+//   }
+
     palavras.stream()
-        .filter(maisDeCincoCaracteres)
+        .filter(p -> p.length() > 5)
         .forEach(System.out::println);
   }
 }
